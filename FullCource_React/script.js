21192de488efd1a16ec6c13b61502403d271da23
Nikolay_Kolotilo <3dnikik00@gmail.com>
2022-10-17 23:31:05 +0300

    Практика ч.1 Начинаем создавать приложение.

--- FullCource_React/script.js
+++ FullCource_React/script.js
@@ -1,8 +1,16 @@
-let admin;
+const numberOfFilms = +prompt('сколько фильмов вы уже просмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const lastFilm = prompt('Один из последних просмотренных фильмов', ''),
+    gradeOfLastFilm = +prompt('На сколько оцените его', ''),
+    lastFilm2 = prompt('Один из последних просмотренных фильмов', ''),
+    gradeOfLastFilm2 = +prompt('На сколько оцените его', '');
+personalMovieDB.movies[lastFilm] = gradeOfLastFilm;
+personalMovieDB.movies[lastFilm2] = gradeOfLastFilm2;
 
-let name;
-name = "Djon";
-
-admin = name ;
-
-alert(admin);
+console.log(personalMovieDB);
