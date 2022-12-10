af28ef0d886f29630ba74b77a984308cd9c0420b
Nikolay_Kolotilo <3dnikik00@gmail.com>
2022-12-10 23:59:55 +0200

    Приложение по лекцию №80 Включительно.

--- FullCource_React/Food/js/script.js
+++ FullCource_React/Food/js/script.js
@@ -153,15 +153,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     //// Лекция 79. Карточки продуктов меню.
-            //Класс для удаления меню.
-    
+
+            //Класс для удаления меню (Не по видео).    
     class MenuCardDelete {
         constructor(menuCardSelector) {
             this.menuCardSelector = menuCardSelector;
             this.menuItemIs = document.querySelectorAll(this.menuCardSelector);
         }
         del() {
-            console.log(this.menuItemIs);
             this.menuItemIs.forEach(item => {
                 item.remove();
             });
@@ -171,12 +170,13 @@ window.addEventListener('DOMContentLoaded', () => {
     
             //Классы для карточек.
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {//// Лекция 80. 7:40;
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAN();            
@@ -186,16 +186,20 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.classes = 'menu__item';
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }            
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.title} - ${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.title} - ${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
                 `;
             this.parent.append(element);
@@ -207,7 +211,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
     ).render();
 
     new MenuCard(
