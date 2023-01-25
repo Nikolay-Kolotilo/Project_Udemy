d3412814585336c7559c885451fe8f381fc12411
Nikolay_Kolotilo <3dnikik00@gmail.com>
2023-01-25 23:49:06 +0200

    Lec_109__lib of Jquery

---
+++ FullCource_React/Lectures/Lec_109__lib of Jquery.js
@@ -0,0 +1,8 @@
+'use strict';
+/*109__Библиотека Jquery. https://Jquery.page2page.ru - Справка по Jquery.
+1:30.   https://cdnjs.com/libraries/jquery
+        slim - укороченая версия.
+2:20. Настройка webpack.config.js для Jquery.
+3:00. Применение в скрипте
+6:00. Импорт в скрпт с $.
+*/
