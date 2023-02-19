2b011ed7bd5323c60d25f686ceb59b548907ace1
Nikolay_Kolotilo <3dnikik00@gmail.com>
2023-02-19 02:33:43 +0200

    Lec_139__React_Semantics and content accessibility

---
+++ FullCource_React/Lectures/Lec_139__React_Semantics and content accessibility.js
@@ -0,0 +1,20 @@
+/*Lec_139__React_Semantics and content accessibility.js
+        https://ru.reactjs.org/docs/accessibility.html
+        http://prgssr.ru/development/ispolzovanie-aria-v-html5.html
+        https://developer.mozilla.org/ru/docs/Web/Accessibility/ARIA
+        https://ru.wikipedia.org/wiki/%D0%AD%D0%BA%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5_%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0%B0%D1%8E%D1%89%D0%B5%D0%B5_%D1%83%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE
+        https://www.youtube.com/watch?v=KAK-WAb9vow&ab_channel=%D0%92%D0%B5%D0%B1-%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D1%8B
+        https://www.youtube.com/watch?v=bDYEnNzprzE&ab_channel=%D0%92%D0%B5%D0%B1-%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D1%8B
+
+0:15. Использование Header, Footer и т.д. 
+1:00. Что б не ломать верстку - используем Реакт фрагменты. Соблюдение иерархии заголовков, использование alt в Тегах. 
+2:40. Использование атрибутов Aria. пишутся как aria-name эти атрибуты это исключение в Реакте. 
+3:45. Использование alt атрибутов для картинок. 
+4:20. Пользование сайтом без мышки - корректная работа TAB, Shift+TAB, подсвечивание кнопок по умолчанию (outline). 
+outline это свойство кнопки по умолчанию. 
+6:50. Можно проверить сайт самому с клавиатуры. 
+7:10. TABIndex - контроль работы с выделением для DIV, SPAN. 
+
+
+
+
